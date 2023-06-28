aa5a08703e14e4b305a7b8f4d5abfb6b9f134162
xl <40017853+sakatomato@users.noreply.github.com>
2023-06-28 15:39:04 +0800

    first scrool and mv

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,145 @@
+import { useEffect } from 'react'
+import { gsap } from 'gsap'
+import './app.scss'
+
+const mv = {
+    isActive: true,
+    isMoving: false,
+}
+
+const pageScrollFromMv = () => {
+    if (mv.isMoving) return
+    mv.isMoving = true
+    const tl = gsap.timeline()
+    tl.to('.mv', {
+        y: '-5%',
+        duration: 0.95,
+        opacity: 0,
+    })
+        .to('.mv', {
+            height: 0,
+            duration: 0.05,
+            ease: 'power3.out',
+        })
+        .to('.content', {
+            height: '100vh',
+            duration: 2,
+            ease: 'power3.in',
+            onComplete: () => {
+                mv.isMoving = false
+                mv.isActive = false
+            },
+        })
+}
+
+const pageScrollFromContent = () => {
+    if (mv.isMoving) return
+    mv.isMoving = true
+    const tl = gsap.timeline()
+    tl.to('.content', {
+        height: 0,
+        duration: 1,
+        ease: 'back.out',
+    }).fromTo(
+        '.mv',
+        {
+            height: '100vh',
+            opacity: 0,
+            duration: 0.05,
+        },
+        {
+            opacity: 1,
+            duration: 0.95,
+            ease: 'back.in',
+            onComplete: () => {
+                mv.isMoving = false
+                mv.isActive = true
+            },
+        }
+    )
+}
+const handleOnePageScrollKey = (e: React.KeyboardEvent<Element>) => {
+    if (e.repeat) return
+
+    const key = e.key
+    if (mv.isActive && key === 'ArrowDown') pageScrollFromMv()
+    else if (!mv.isActive && key === 'ArrowUp') pageScrollFromContent()
+}
+const handleWheelPageScroll = (e: WheelEvent) => {
+    const delta = e.deltaY
+    if (mv.isActive && delta > 0) {
+        pageScrollFromMv()
+    } else if (!mv.isActive && delta < 0) {
+        pageScrollFromContent()
+    }
+}
+
+let prevTouchY = 0
+const touchRatio = 10
+const setFirstTouchY = (e: TouchEvent) => {
+    prevTouchY = e.touches[0].clientY
+}
+const handleTouchMovePageScroll = (e: TouchEvent) => {
+    const touchY = e.touches[0].clientY
+    const delta = touchY - prevTouchY
+    if (mv.isActive && delta < -touchRatio) {
+        pageScrollFromMv()
+    } else if (!mv.isActive && delta > touchRatio) {
+        pageScrollFromContent()
+    }
+    prevTouchY = touchY
+}
+
+const initMvContent = () => {
+    const observerEl = document.querySelector('.observe-point')
+    if (!observerEl) return
+    const callback: IntersectionObserverCallback = (_entries) => {
+        _entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                window.addEventListener('wheel', handleWheelPageScroll)
+
+                // touch
+                window.addEventListener('touchstart', setFirstTouchY)
+                window.addEventListener('touchmove', handleTouchMovePageScroll)
+
+                window.addEventListener(
+                    'keydown',
+                    handleOnePageScrollKey as any
+                )
+            } else {
+                window.removeEventListener('wheel', handleWheelPageScroll)
+            }
+        })
+    }
+    const observer = new IntersectionObserver(callback)
+    observer.observe(observerEl)
+}
+const initMv = () => {
+    const mvTimeLine = gsap.timeline({ onComplete: initMvContent })
+
+    mvTimeLine
+        .fromTo(
+            '.mv-title',
+            { opacity: 0 },
+            { opacity: 1, ease: 'sine', duration: 1, delay: 1 }
+        )
+        .fromTo(
+            '.mv-sub-title',
+            { opacity: 0 },
+            { opacity: 1, ease: 'sine', duration: 1 }
+        )
+}
+
 function App() {
+    useEffect(() => {
+        initMv()
+    }, [])
+
     return (
         <>
             <div className="mv">
-                <h1 className="mv__title">タイトル</h1>
-                <p className="mv__sub-title">サブタイトル</p>
+                <h1 className="mv-title">Saka Tomato</h1>
+                <h3 className="mv-sub-title">...关于坂本西红柿的一切</h3>
             </div>
             <div className="content">
                 <div className="observe-point"></div>
